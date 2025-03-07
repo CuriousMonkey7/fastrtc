c920f61ecb6f29ac728ebd077dd8b2c1d7245e01
Sourabh <Sourabh72101@gmail.com>
2025-03-08 03:53:25 +0530

    Adding HumAware VAD model

--- backend/fastrtc/pause_detection/__init__.py
+++ backend/fastrtc/pause_detection/__init__.py
@@ -1,7 +1,9 @@
 from .protocol import ModelOptions, PauseDetectionModel
 from .silero import SileroVADModel, SileroVadOptions, get_silero_model
-
+from .humaware import HumAwareVADModel, HumAwareVADOptions
 __all__ = [
+    "HumAwareVADModel",
+    "HumAwareVADOptions",
     "SileroVADModel",
     "SileroVadOptions",
     "PauseDetectionModel",
